a231a4db4dbd524f3b61df3cf4fafad6ea2dc54a
Andrew Hudzenko <s1mpleworkpost@gmail.com>
2023-11-30 20:31:42 +0200

    create customer module + docker-compose.yml

---
+++ customer/src/main/java/com/amigoscode/customer/model/Customer.java
@@ -0,0 +1,28 @@
+package com.amigoscode.customer.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "custom_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "custom_id_sequence"
+    )
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ customer/src/main/java/com/amigoscode/customer/service/CustomerService.java
@@ -0,0 +1,20 @@
+package com.amigoscode.customer.service;
+
+import com.amigoscode.customer.dto.CustomerRegistrationRequest;
+import com.amigoscode.customer.model.Customer;
+import com.amigoscode.customer.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        // todo: check if email valid
+        // todo: check if email not taken
+        customerRepository.save(customer);
+    }
+}
