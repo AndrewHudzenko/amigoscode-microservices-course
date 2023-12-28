2d7055b0d4ab2ea397319442b029977a62d42465
Andrew Hudzenko <s1mpleworkpost@gmail.com>
2023-12-28 13:39:14 +0200

    add k8s configuration

--- clients/src/main/java/com/amigoscode/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/amigoscode/clients/notification/NotificationClient.java
@@ -4,7 +4,10 @@ import com.amigoscode.clients.notification.dto.NotificationRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name = "notification",
+        url = "${clients.notification.url}"
+)
 public interface NotificationClient {
 
     @PostMapping("api/v1/notification")
